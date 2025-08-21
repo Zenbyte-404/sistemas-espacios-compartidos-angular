57735ddcda46d741bd09f2c2e6cfcfa66568d006
Nico0626 <nico.menon.2@gmail.com>
2025-08-21 12:25:57 -0300

    Agrego componente objetivo y carpeta de assets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import { Objetivo } from './page/objetivo/objetivo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet,Header,Footer,Objetivo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/page/objetivo/objetivo.ts
+++ src/app/page/objetivo/objetivo.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-objetivo',
