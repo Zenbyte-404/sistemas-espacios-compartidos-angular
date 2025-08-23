3700208053bcabfbc2fef3a8a5a67477e95f52d3
Agustin <agusgibaut693@gmail.com>
2025-08-23 13:53:33 -0300

    agrego enrutamiento del nav con el login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './page/home/home';
 import { Objetivo } from './page/objetivo/objetivo';
 import { QuienesSomos } from './page/quienes-somos/quienes-somos';
+import { Login } from './page/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'objetivo', component: Objetivo },
-  { path: 'quienes-somos', component: QuienesSomos }
+  { path: 'quienes-somos', component: QuienesSomos },
+  { path: 'login', component: Login } 
 ];
--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,            // <- necesario para usarlo en imports
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrls: ['./footer.css']   // <- corregido styleUrls
 })
-export class Footer {
-
-}
+export class Footer {}
