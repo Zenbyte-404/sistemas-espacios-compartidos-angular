dbc63d5cad98bb49a816ea8f993148c79acb8d87
Jose Tiranti <josi.tiranti@gmail.com>
2025-08-23 01:14:15 -0300

    Actualización del header y menú hamburguesa

--- src/app/shared/header/header.spec.ts
+++ src/app/shared/header/header.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Header } from './header';
 
 describe('Header', () => {
@@ -21,3 +20,4 @@ describe('Header', () => {
     expect(component).toBeTruthy();
   });
 });
+
