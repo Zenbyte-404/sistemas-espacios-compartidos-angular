59a0e10d2dd7e5b163a53c38ac383374e2fcfe9c
Nico0626 <nico.menon.2@gmail.com>
2025-08-24 16:41:00 -0300

    Link cometado a Gestion de datos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './page/home/home';
 import { Objetivo } from './page/objetivo/objetivo';
 import { QuienesSomos } from './page/quienes-somos/quienes-somos';
 import { Login } from './page/login/login';
+import { GestorDatos } from './page/gestor-datos/gestor-datos';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'objetivo', component: Objetivo },
   { path: 'quienes-somos', component: QuienesSomos },
-  { path: 'login', component: Login } 
+  { path: 'login', component: Login }, 
+  {path: 'gestor-datos', component: GestorDatos}
 ];
--- src/app/page/objetivo/objetivo.ts
+++ src/app/page/objetivo/objetivo.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-objetivo',
   standalone: true,
-  imports: [Header, Footer],
+  imports: [Header, Footer,RouterModule],
   templateUrl: './objetivo.html',
   styleUrls: ['./objetivo.css'] // Corrección: debe ser styleUrls (plural)
 })
