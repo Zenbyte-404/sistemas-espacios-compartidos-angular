c9618e6f378a0a9540590140c9ca711a42e519df
Nico0626 <nico.menon.2@gmail.com>
2025-08-24 18:55:39 -0300

    Agrego componentes layout, divido dashboard de paginas publicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,32 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
+
 import { Home } from './page/home/home';
 import { Objetivo } from './page/objetivo/objetivo';
 import { QuienesSomos } from './page/quienes-somos/quienes-somos';
 import { Login } from './page/login/login';
 import { GestorDatos } from './page/gestor-datos/gestor-datos';
 import { Dashboard } from './page/dashboard/dashboard';
+import { from } from 'rxjs';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'objetivo', component: Objetivo },
-  { path: 'quienes-somos', component: QuienesSomos },
-  { path: 'login', component: Login }, 
-  {path: 'gestor-datos', component: GestorDatos},
-  {path:'dashboard',component:Dashboard}
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: '', component: Home },
+      { path: 'objetivo', component: Objetivo },
+      { path: 'quienes-somos', component: QuienesSomos },
+      { path: 'login', component: Login },
+      { path: 'gestor-datos', component: GestorDatos }
+    ]
+  },
+  {
+    path: 'dashboard',
+    component: DashboardLayout,
+    children: [
+      { path: '', component: Dashboard }
+    ]
+  }
 ];
