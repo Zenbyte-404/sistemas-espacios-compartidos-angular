5a4ef56b0bb6b75c164237095fafbfd482d3e28f
Nico0626 <nico.menon.2@gmail.com>
2025-09-06 15:51:05 -0300

    Arreglo Error

--- src/app/page/error-404/error-404.ts
+++ src/app/page/error-404/error-404.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error-404',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './error-404.html',
   styleUrl: './error-404.css'
 })
