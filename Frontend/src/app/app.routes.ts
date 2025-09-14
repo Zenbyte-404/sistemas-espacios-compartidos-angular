c0cc2e6755433db55a1c225e4b728bec0074a8a4
Nico0626 <nico.menon.2@gmail.com>
2025-09-13 21:14:38 -0300

    Arreglo importaciones en app.router

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
 
-import { Home } from './page/home/home';
-import { Objetivo } from './page/objetivo/objetivo';
-import { QuienesSomos } from './page/quienes-somos/quienes-somos';
-import { Login } from './page/login/login';
-import { GestorDatos } from './page/gestor-datos/gestor-datos';
+import { Home } from './page/public/home/home';
+import { Objetivo } from './page/info/objetivo/objetivo';
+import { QuienesSomos } from './page/info/quienes-somos/quienes-somos';
+import { Login } from './page/autenticacion/login/login';
+import { GestorDatos } from './page/dashboard/componentes/gestor-datos/gestor-datos';
 import { Dashboard } from './page/dashboard/dashboard';
 import { from } from 'rxjs';
-import { Error404 } from './page/error-404/error-404';
+import { Error404 } from './page/error/error-404/error-404';
 
 export const routes: Routes = [
   {
