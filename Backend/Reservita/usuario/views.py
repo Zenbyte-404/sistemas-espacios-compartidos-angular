9a277c503d84a41babe24fe5f2ab797de78c3753
Nico0626 <nico.menon.2@gmail.com>
2025-10-15 02:41:28 -0300

    Se agreag modulo Usuario/conexion con localhost4200

--- Backend/Reservita/usuario/views.py
+++ Backend/Reservita/usuario/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Usuario
+from .serializers import UsuarioSerializer
 
-# Create your views here.
+# CRUD normal (listar, crear, actualizar, borrar)
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+# Registro
+@api_view(['POST'])
+def register(request):
+    serializer = UsuarioSerializer(data=request.data)
+    if serializer.is_valid():
+        usuario = serializer.save()
+        user_data = UsuarioSerializer(usuario).data
+        return Response({"user": user_data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Login
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    try:
+        usuario = Usuario.objects.get(email=email, password=password)
+        user_data = UsuarioSerializer(usuario).data
+        return Response({"user": user_data})
+    except Usuario.DoesNotExist:
+        return Response({"error": "Credenciales inválidas"}, status=status.HTTP_401_UNAUTHORIZED)
